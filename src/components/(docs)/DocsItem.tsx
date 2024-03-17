85ec8b6b78f5e4888a603153cdd79bb46abf2796
Pranshu Patel <pranshu05patel@gmail.com>
2024-03-17 19:25:54 +0530

    Refactor DocsItem and NavBar components

--- src/components/(docs)/DocsItem.tsx
+++ src/components/(docs)/DocsItem.tsx
@@ -10,16 +10,19 @@ interface DocsItemProps {
 }
 
 const DocsItem: React.FC<DocsItemProps> = ({ slug, title, date, description, viewCount, img }) => (
-    <Link href={`/docs/${slug}`} passHref className='flex flex-col rounded-lg border border-neutral-700 p-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:bg-neutral-800/30'>
-        <div className='w-full flex flex-col mt-2'>
-            <div className='w-full flex justify-between mb-2'>
-                <span className='text-zinc-400 text-sm'>{date}</span>
-                <span className='text-zinc-400 text-sm'>{viewCount} views</span>
-            </div>
-            <span className='text-xl font-bold'>{title}</span>
-            <span className='text-zinc-400'>{description}</span>
+    <div className="group relative">
+        <div className="absolute -inset-1 rounded-lg bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 opacity-50 blur transition duration-500 group-hover:opacity-100"></div>
+        <div className='relative rounded-lg bg-black px-7 py-4 text-white'>
+            <Link href={`/docs/${slug}`} passHref className='w-full flex flex-col'>
+                <div className='w-full flex justify-between mb-2'>
+                    <span className='text-zinc-400 text-sm'>{date}</span>
+                    <span className='text-zinc-400 text-sm'>{viewCount} views</span>
+                </div>
+                <span className='text-xl font-bold'>{title}</span>
+                <span className='text-zinc-400'>{description}</span>
+            </Link>
         </div>
-    </Link>
+    </div>
 )
 
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,18 @@ import Link from "next/link";
 export default function Home() {
     return (
         <div className="w-11/12 md:w-4/5 lg:w-3/4 xl:w-2/3 2xl:w-1/2 mx-auto flex flex-col items-center justify-between py-20 h-full">
-            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-                <h1 className="text-5xl font-bold text-center">DataStructures</h1>
-            </div>
-
-            <div className="w-full rounded-lg flex flex-col my-10">
-                <Link href="/docs" className="rounded-lg border border-transparent p-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-                    <h2 className="mb-3 text-2xl font-semibold">
-                        Docs{" "}<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                    </h2>
-                    <p className="m-0 text-sm opacity-50">Find in-depth information about DataStructures using CPP.</p>
-                </Link>
+            <h1 className="text-5xl font-bold text-center">DataStructures</h1>
+            <p className="text-center text-lg opacity-50 mb-4">A collection of DataStructures using CPP</p>
+            <div className="group relative mt-10 w-full">
+                <div className="absolute -inset-1 rounded-lg bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 opacity-50 blur transition duration-500 group-hover:opacity-100"></div>
+                <div className="relative rounded-lg bg-black px-7 py-4 text-white">
+                    <Link href="/docs" className="w-full flex flex-col">
+                        <h2 className="mb-3 text-2xl font-semibold">
+                            Docs{" "}<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
+                        </h2>
+                        <p className="m-0 text-sm opacity-50">Find in-depth information about DataStructures using CPP.</p>
+                    </Link>
+                </div>
             </div>
         </div>
     );
