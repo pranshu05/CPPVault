97de584815836b272e600cc776187dd178815c6d
Pranshu Patel <pranshu05patel@gmail.com>
2024-03-18 13:32:08 +0530

    Refactor code: Move serialize import and add useRouter import***

--- src/pages/docs/[category]/[slug]/index.tsx
+++ src/pages/docs/[category]/[slug]/index.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
-import { serialize } from 'next-mdx-remote/serialize';
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import { serialize } from 'next-mdx-remote/serialize';
 import rehypeHighlight from 'rehype-highlight';
+import { useRouter } from 'next/router';
 
 import MetaInfo from '@/components/(docs)/(category)/(slug)/MetaInfo';
 import DocsContent from '@/components/(docs)/(category)/(slug)/DocsContent';
@@ -11,7 +12,6 @@ import DocsFooter from '@/components/(docs)/(category)/(slug)/DocsFooter';
 import { getViewCount, incrementViewCount } from '../../../../lib/ViewsData';
 
 import langCPP from "highlight.js/lib/languages/cpp"
-
 import 'highlight.js/styles/github-dark.css'
 
 export const languages = {
@@ -24,6 +24,7 @@ interface Frontmatter {
     description: string;
     readTime: number;
     img: string;
+    category: string;
 }
 
 interface DocsProps {
@@ -37,7 +38,8 @@ interface DocsProps {
 }
 
 const Docs: React.FC<DocsProps> = ({ frontMatter, mdxSource }) => {
-    const { slug, title, date, description, readTime, img } = frontMatter;
+    const { slug, title, date, description, readTime, img, category } = frontMatter;
+
     const [viewCount, setViewCount] = useState<number | null>(null);
 
     useEffect(() => {
@@ -71,16 +73,24 @@ export async function getStaticPaths() {
     const docsDirectory = path.join(process.cwd(), 'src', 'docs');
     const docs = fs.readdirSync(docsDirectory);
 
-    const paths = docs.map((doc) => ({
-        params: { category: '', slug: doc.replace(/\.mdx$/, '') },
-    }));
+    const paths = docs.map((doc) => {
+        const fileName = doc.replace(/\.mdx$/, '');
+        const filePath = path.join(docsDirectory, doc);
+        const fileContent = fs.readFileSync(filePath, 'utf-8');
+        const { data } = matter(fileContent);
+
+        const category = encodeURIComponent(data.category || 'Uncategorized');
+        return {
+            params: { category, slug: fileName },
+        };
+    });
 
     return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }: { params: { category: string; slug: string } }) {
     const { category, slug } = params;
-    const filePath = path.join(process.cwd(), 'src', 'docs', category, `${slug}.mdx`);
+    const filePath = path.join(process.cwd(), 'src', 'docs', `${slug}.mdx`);
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const { data: frontMatter, content } = matter(fileContent);
 
